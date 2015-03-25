fe40149991d4f2a222b16293687445c611aa1c10
Ryan Hightower <ryan@ryanhightower.com>
2015-03-25 14:23:02 -0600

    Added gallery

--- app.js
+++ app.js
@@ -7,23 +7,29 @@
 		this.products = products;
 	});
 	
-	// 1. Let's create a new controller to make Tabs that will show different product information (description, specs, reviews, etc.)
 	app.controller('TabController', function(){
-		
-		// 2. define the default tab
 		this.tab = 1;
-
-		// 3. create a function to set the tab.
 		this.setTab = function(setTab){
 			this.tab = setTab;
 		};
 
-		// 4. create a function that checks if a tab is set (it will return a boolean).
 		this.isSet = function(tab){
 			return tab===this.tab;
 		};
 
-		// 5. now go connect your tabs to the HTML
+	});
+
+	// 1. Now let's create a Gallery controller that will control our thumbnails.
+	// This will be a lot like our TabController.
+	app.controller('GalleryController', function(){
+		// 2. let's store the value of the current image from the image array (remember, we're using an array so the first image index is 0).
+		this.current = 0;
+		// 3. create a function that will let us set the current photo (like we set the Tabs).
+		this.setCurrent = function(current){
+			this.current = current || 0;
+		};
+
+		// Now we can hook up our gallery in our HTML.
 	});
 
 
@@ -33,11 +39,14 @@
 			price: 2,
 			description: 'great Dodeca gem!',
 			images:[{
-				full: 'http://placekitten.com/g/300',
-				thumb: 'http://placekitten.com/g/150'
+				full: 'http://placekitten.com/g/300/300',
+				thumb: 'http://placekitten.com/g/300/300'
 			},{
-				full: 'http://placekitten.com/g/310',
-				thumb: 'http://placekitten.com/g/140'
+				full: 'http://placekitten.com/g/302',
+				thumb: 'http://placekitten.com/g/302'
+			},{
+				full: 'http://placekitten.com/g/304',
+				thumb: 'http://placekitten.com/g/304'
 			}],
 			canPurchase: true, 
 			soldOut: false,
@@ -46,11 +55,14 @@
 			price: 5.95,
 			description: 'great Pentagonal gem!',
 			images:[{
-				full: 'http://placekitten.com/g/300',
-				thumb: 'http://placekitten.com/g/150'
+				full: 'http://placekitten.com/g/300/300',
+				thumb: 'http://placekitten.com/g/300/300'
 			},{
-				full: 'http://placekitten.com/g/310',
-				thumb: 'http://placekitten.com/g/140'
+				full: 'http://placekitten.com/g/302',
+				thumb: 'http://placekitten.com/g/302'
+			},{
+				full: 'http://placekitten.com/g/304',
+				thumb: 'http://placekitten.com/g/304'
 			}],
 			canPurchase: false, 
 			soldOut: false,
@@ -59,11 +71,14 @@
 			price: 10.95,
 			description: 'great Diamond gem!',
 			images:[{
-				full: 'http://placekitten.com/g/300',
-				thumb: 'http://placekitten.com/g/150'
+				full: 'http://placekitten.com/g/300/300',
+				thumb: 'http://placekitten.com/g/300/300'
 			},{
-				full: 'http://placekitten.com/g/310',
-				thumb: 'http://placekitten.com/g/140'
+				full: 'http://placekitten.com/g/302',
+				thumb: 'http://placekitten.com/g/302'
+			},{
+				full: 'http://placekitten.com/g/304',
+				thumb: 'http://placekitten.com/g/304'
 			}],
 			canPurchase: true, 
 			soldOut: false,
