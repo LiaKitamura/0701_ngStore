eb6cb67fcbdb0ed5d96715c60130be78d8ad7773
Ryan Hightower <ryan@ryanhightower.com>
2015-03-25 16:20:47 -0600

    minor

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
 	// angular.module( 'applicationName', [ dependencies, commaSeparated, list ])
 	var app = angular.module('store', []);
 
-	// 1. add a controller to store the gem info
+	// 1. add a controller to store the product info
 	app.controller('StoreController', function(){
-		// 2. add the gem as a *property* of the StoreController
-		this.product = gem;
+		// 2. add the product as a *property* of the StoreController
+		this.product = product;
 
 		// 3. now go add the StoreController to the HTML
 	});
 
-	var gem = {
+	var product = {
 		name: 'Dodecahedron',
 		price: 2.95,
 		description: 'great Dodeca gem!',
