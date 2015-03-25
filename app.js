d19b311ec23aedf6f0ca25244cb7f151769865ce
Ryan Hightower <ryan@ryanhightower.com>
2015-03-25 11:33:47 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Wrap app in an anonymous function just as a good practice.
+(function(){
+
+	// angular.module( 'applicationName', [ dependencies, commaSeparated, list ])
+	var app = angular.module('store', []);
+
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'great Dodeca gem!',
+	}
+
+})();
