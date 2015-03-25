08796048282e057f6396c18ce1c66d1959bf34d1
Ryan Hightower <ryan@ryanhightower.com>
2015-03-25 16:20:09 -0600

    removed old comments.

--- app.js
+++ app.js
@@ -26,28 +26,12 @@
 		};
 	});
 
-	// 1. 	Let's clean up our code a little by adding a new controller for our Reviews. 
-	// 		That way we can define our 'review' var that we used in the HTML.
 	app.controller('ReviewController', function(){
-		// 1.1 	We'll initialize the review as a blank Object.
 		this.review = {};
 
-		// 1.2	Now we can go add this controller to the review form.
-
-		// 3. 	Now we can create a function to add a new review when the form is submitted
-		// 3.1 	We'll pass in a product so we know which product Object to add the review to.
 		this.addReview = function(product){
-			// 3.2 	Then we can simply access the reviews property of that product and .push the review onto the array.
-			// 		This works because we bound the form's review model to our ReviewController. So that model is constantly 
-			// 		being watched as we input new information.
 			product.reviews.push(this.review);
-
-			// 3.3 	Make sure to clear the .review Object after it has been submitted! 
-			// 		After it is submitted, we want to be done with it and start a new review. This will also clear the form
-			// 		because, again, the Object is bound to that form through ng-model.
 			this.review = {};
-
-			// 3.4 	Now go add this function to the form.
 		};
 	});
 
