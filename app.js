30cff71924dbe86a42bf6f96ecfa18a36def4d6e
Ryan Hightower <ryan@ryanhightower.com>
2015-03-25 11:44:24 -0600

    adding a controller

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 	// angular.module( 'applicationName', [ dependencies, commaSeparated, list ])
 	var app = angular.module('store', []);
 
+	// 1. add a controller to store the gem info
+	app.controller('StoreController', function(){
+		// 2. add the gem as a *property* of the StoreController
+		this.product = gem;
+
+		// 3. now go add the StoreController to the HTML
+	});
+
 	var gem = {
 		name: 'Dodecahedron',
 		price: 2.95,
