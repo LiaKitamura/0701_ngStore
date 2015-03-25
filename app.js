e8ffc3fb1278c08f3362a0ec3fc1e0535fa6cbd4
Ryan Hightower <ryan@ryanhightower.com>
2015-03-25 13:19:03 -0600

    added images

--- app.js
+++ app.js
@@ -5,28 +5,50 @@
 
 	app.controller('StoreController', function(){
 
-		this.products = gems;
+		this.products = products;
 
 	});
-
-	var gems = [{
+	// 1. Let's add images to our products
+	var products = [{
 			name: 'Dodecahedron',
 			price: 2,
 			description: 'great Dodeca gem!',
+			images:[{
+				full: 'http://placekitten.com/g/300',
+				thumb: 'http://placekitten.com/g/150'
+			},{
+				full: 'http://placekitten.com/g/310',
+				thumb: 'http://placekitten.com/g/140'
+			}],
 			canPurchase: true, 
 			soldOut: false,
 		},{
 			name: 'Pentagonal Gem',
 			price: 5.95,
 			description: 'great Pentagonal gem!',
+			images:[{
+				full: 'http://placekitten.com/g/300',
+				thumb: 'http://placekitten.com/g/150'
+			},{
+				full: 'http://placekitten.com/g/310',
+				thumb: 'http://placekitten.com/g/140'
+			}],
 			canPurchase: false, 
 			soldOut: false,
 		},{
 			name: 'Diamond',
 			price: 10.95,
 			description: 'great Diamond gem!',
+			images:[{
+				full: 'http://placekitten.com/g/300',
+				thumb: 'http://placekitten.com/g/150'
+			},{
+				full: 'http://placekitten.com/g/310',
+				thumb: 'http://placekitten.com/g/140'
+			}],
 			canPurchase: true, 
 			soldOut: false,
 		}]
+		// 2. now go add images to html
 
 })();
